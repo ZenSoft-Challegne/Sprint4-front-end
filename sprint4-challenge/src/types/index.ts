0d9057c436c22fb6eb4129bc499a71321f9fa2e7
Vircsss <virc06@gmail.com>
2025-11-09 19:28:04 -0300

    fix: arrumando o type da EtapaJornada.

--- sprint4-challenge/src/types/index.ts
+++ sprint4-challenge/src/types/index.ts
@@ -1,29 +1,30 @@
 export interface Paciente {
-    id: string;
-    nome: string;
-    email: string;
-    telefone: string;
-    cpf: string;
+  id: string;
+  nome: string;
+  email: string;
+  telefone: string;
+  cpf: string;
 }
 
 export interface Medico {
-    id: string;
-    nome: string;
-    especialidade: string;
-    crm: string;
+  id: string;
+  nome: string;
+  especialidade: string;
+  crm: string;
 }
 
 
 export interface Teleconsulta {
-    id: string;
-    paciente: Paciente;
-    medico: Medico;
-    dataHora: string;
-    sintomas?: string;
-    diagnostico?: string;
-    prescricao?: string;
-    feedback?: string;
-    avaliacao?: number;
+  id: string;
+  paciente: Paciente;
+  medico: Medico;
+  dataHora: string;
+
+  sintomas?: string;
+  diagnostico?: string;
+  prescricao?: string;
+  feedback?: string;
+  avaliacao?: number;
 }
 
 export type NovaConsulta = Omit<Teleconsulta, 'id' | 'status'>;
@@ -31,9 +32,8 @@ export type NovaConsulta = Omit<Teleconsulta, 'id' | 'status'>;
 export type AtualizarConsulta = Partial<Omit<Teleconsulta, 'id'>>;
 
 export interface EtapaJornada {
-    id: number;
-    titulo: string;
-    descricao: string;
-    icon: string;
-    status: 'concluida' | 'ativa' | 'pendente';
+  id: number;
+  titulo: string;
+  descricao: string;
+  status: 'concluida' | 'ativa' | 'pendente';
 }
